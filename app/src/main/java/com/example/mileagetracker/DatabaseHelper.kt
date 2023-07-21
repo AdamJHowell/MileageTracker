31bdc7f6ccd62054e77264d64c48c2521647e793
Adam Howell <AdamJHowell@users.noreply.github.com>
2023-07-20 22:52:12 -0600

    Converted more fields to match this app's purpose.

--- app/src/main/java/com/example/mileagetracker/DatabaseHelper.kt
+++ app/src/main/java/com/example/mileagetracker/DatabaseHelper.kt
@@ -7,21 +7,21 @@ import android.database.sqlite.SQLiteOpenHelper
 
 class DatabaseHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION)
 {
-
 	companion object
 	{
-		private const val DATABASE_NAME = "my_database.db"
+		private const val DATABASE_NAME = "mileage-tracker.db"
 		private const val DATABASE_VERSION = 1
-		private const val TABLE_NAME = "my_table"
-		private const val COLUMN_NAME = "name"
-		private const val COLUMN_EMAIL = "email"
-		private const val COLUMN_AGE = "age"
-		private const val COLUMN_SCORE = "score"
+		private const val TABLE_NAME = "fill-up-records"
+		private const val COLUMN_ODOMETER = "odometer"
+		private const val COLUMN_DATE = "date"
+		private const val COLUMN_VOLUME = "volume"
+		private const val COLUMN_PRICE = "price"
 	}
 
 	override fun onCreate(db: SQLiteDatabase)
 	{
-		val createTableQuery = "CREATE TABLE $TABLE_NAME (" + "_id INTEGER PRIMARY KEY AUTOINCREMENT, " + "$COLUMN_NAME TEXT, " + "$COLUMN_EMAIL TEXT, " + "$COLUMN_AGE INTEGER, " + "$COLUMN_SCORE REAL" + ");"
+		val createTableQuery =
+			"CREATE TABLE $TABLE_NAME (_id INTEGER PRIMARY KEY AUTOINCREMENT, $COLUMN_ODOMETER TEXT, $COLUMN_DATE TEXT, $COLUMN_VOLUME INTEGER, $COLUMN_PRICE REAL);"
 
 		db.execSQL(createTableQuery)
 	}
@@ -32,13 +32,13 @@ class DatabaseHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME
 		onCreate(db)
 	}
 
-	fun insertData(name: String, email: String, age: Int, score: Double): Long
+	fun insertData(odometer: String, date: String, volume: Int, price: Double): Long
 	{
 		val values = ContentValues()
-		values.put(COLUMN_NAME, name)
-		values.put(COLUMN_EMAIL, email)
-		values.put(COLUMN_AGE, age)
-		values.put(COLUMN_SCORE, score)
+		values.put(COLUMN_ODOMETER, odometer)
+		values.put(COLUMN_DATE, date)
+		values.put(COLUMN_VOLUME, volume)
+		values.put(COLUMN_PRICE, price)
 
 		val db = writableDatabase
 		return db.insert(TABLE_NAME, null, values)
